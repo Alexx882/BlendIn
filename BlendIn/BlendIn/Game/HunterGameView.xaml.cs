570f4bee3cc289096377e75814874f8975fd83d4
VeitFrick <veit.frick@aau.at>
2019-09-28 01:20:16 +0200

    Added gameLoopStuff

--- BlendIn/BlendIn/Game/HunterGameView.xaml.cs
+++ BlendIn/BlendIn/Game/HunterGameView.xaml.cs
@@ -11,8 +11,18 @@ using Xamarin.Forms.Xaml;
 namespace BlendIn.Game
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
+
+
+
     public partial class HunterGameView : ContentPage
     {
+        public double exposeCDValue = 20;
+        public double stunCDValue = 30;
+
+        public double exposeCurrent = 0;
+        public double stunCurrent = 0;
+
+
         public HunterGameView()
         {
             InitializeComponent();
@@ -20,19 +30,58 @@ namespace BlendIn.Game
 
         }
 
+        public void Stun_Clicked()
+        {
+            //TODO Do Something Usefull aka actually stun here
+            stunCurrent = stunCDValue;
+            //ButtonStun.IsEnabled = false;
+
+        }
+
+
+        public void Expose_Clicked()
+        {
+            //TODO Do Something Usefull aka actually expose here
+            exposeCurrent = exposeCDValue;
+            //ButtonExpose.IsEnabled = false;
+
+        }
+
+        public void Catch_Clicked()
+        {
+            //TODO Do Something Usefull aka actually catch here
+
+        }
+
         private void HunterLoop()
         {
             while (true)
             {
+                if (exposeCurrent > 0)
+                {
+                    exposeCurrent--;
+                }
+                else
+                {
+                    //ButtonExpose.IsEnabled = true;
+                }
 
-                Device.BeginInvokeOnMainThread(() => { oct_null.Text = GetOctantString(0); });
-                Device.BeginInvokeOnMainThread(() => { oct_eins.Text = GetOctantString(1); });
-                Device.BeginInvokeOnMainThread(() => { oct_zwei.Text = GetOctantString(2); });
-                Device.BeginInvokeOnMainThread(() => { oct_drei.Text = GetOctantString(3); });
-                Device.BeginInvokeOnMainThread(() => { oct_vier.Text = GetOctantString(4); });
-                Device.BeginInvokeOnMainThread(() => { oct_fuenf.Text = GetOctantString(5); });
-                Device.BeginInvokeOnMainThread(() => { oct_sechs.Text = GetOctantString(6); });
-                Device.BeginInvokeOnMainThread(() => { oct_sieben.Text = GetOctantString(7); });
+                if (stunCurrent > 0)
+                {
+                    stunCurrent--;
+                }
+                else
+                {
+                    //ButtonStun.IsEnabled = true;
+                }
+                //Device.BeginInvokeOnMainThread(() => { oct_null.Text = GetOctantString(0); });
+                //Device.BeginInvokeOnMainThread(() => { oct_eins.Text = GetOctantString(1); });
+                //Device.BeginInvokeOnMainThread(() => { oct_zwei.Text = GetOctantString(2); });
+                //Device.BeginInvokeOnMainThread(() => { oct_drei.Text = GetOctantString(3); });
+                //Device.BeginInvokeOnMainThread(() => { oct_vier.Text = GetOctantString(4); });
+                //Device.BeginInvokeOnMainThread(() => { oct_fuenf.Text = GetOctantString(5); });
+                //Device.BeginInvokeOnMainThread(() => { oct_sechs.Text = GetOctantString(6); });
+                //Device.BeginInvokeOnMainThread(() => { oct_sieben.Text = GetOctantString(7); });
 
 
                 Thread.Sleep(1000);
