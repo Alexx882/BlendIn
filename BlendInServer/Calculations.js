5c2b3d9f23752a6887337e55a0b2f89b28f74cf0
VeitFrick <veit.frick@aau.at>
2019-09-27 20:11:56 +0200

    Distance Function

---
+++ BlendInServer/Calculations.js
@@ -0,0 +1,22 @@
+/**
+ * Calculates the distance between two locations
+ * @param {double} lat1
+ * @param {double} lon1
+ * @param {double} lat2
+ * @param {double} lon2
+ */
+function distance(lat1, lon1, lat2, lon2) {
+    var R = 6371e3; // metres
+    var φ1 = lat1.toRadians();
+    var φ2 = lat2.toRadians();
+    var Δφ = (lat2-lat1).toRadians();
+    var Δλ = (lon2-lon1).toRadians();
+
+    var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    var d = R * c;
+    return d;
+}
