d9c4c9f9e52ca7c9aed1d807019414cf262eb9b3
VeitFrick <veit.frick@aau.at>
2019-09-28 00:32:47 +0200

    Stuff Hunter Loop

--- BlendIn/BlendIn/Game/HunterGameView.xaml.cs
+++ BlendIn/BlendIn/Game/HunterGameView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
@@ -15,6 +16,38 @@ namespace BlendIn.Game
         public HunterGameView()
         {
             InitializeComponent();
+            new Thread(() => HunterLoop()).Start();
+
+        }
+
+        private void HunterLoop()
+        {
+            while (true)
+            {
+
+                Device.BeginInvokeOnMainThread(() => { oct_null.Text = GetOctantString(0); });
+                Device.BeginInvokeOnMainThread(() => { oct_eins.Text = GetOctantString(1); });
+                Device.BeginInvokeOnMainThread(() => { oct_zwei.Text = GetOctantString(2); });
+                Device.BeginInvokeOnMainThread(() => { oct_drei.Text = GetOctantString(3); });
+                Device.BeginInvokeOnMainThread(() => { oct_vier.Text = GetOctantString(4); });
+                Device.BeginInvokeOnMainThread(() => { oct_fuenf.Text = GetOctantString(5); });
+                Device.BeginInvokeOnMainThread(() => { oct_sechs.Text = GetOctantString(6); });
+                Device.BeginInvokeOnMainThread(() => { oct_sieben.Text = GetOctantString(7); });
+
+
+                Thread.Sleep(1000);
+            }
+        }
+
+        private string GetOctantString(int octant)
+        {
+            string s = "";
+            for (int i = 0; i < GameLogic.GetAmountOfPlayersInOctant(octant); i++)
+            {
+                s += "*";
+            }
+            return s;
         }
     }
+
 }
