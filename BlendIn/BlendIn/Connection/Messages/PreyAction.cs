fe849c143c7ca4cf534d30894bd1cff13aad2522
VeitFrick <veit.frick@aau.at>
2019-09-28 18:11:28 +0200

    Blub

---
+++ BlendIn/BlendIn/Connection/Messages/PreyAction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlendIn.Connection.Messages
+{
+    public class PreyAction : BaseMessage
+    {
+        public string lobby { get; set; }
+        public string username { get; set; }
+    }
+}
